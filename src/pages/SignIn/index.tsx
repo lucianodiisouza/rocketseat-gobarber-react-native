05e90277cb617b6ff42b21b8f6394c88a5a719b2
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-11-15 00:37:18 -0300

    using iphone-x-helper to define a different padding-bottom depends on device. Add createAccount component

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,51 @@
 import React from "react";
 import { Image } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
 import logoImg from "../../assets/logo.png";
 import { Input, Button } from "../../components";
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from "./styles";
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
-      <Input name="email" icon="mail" placeholder="Email" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+    <>
+      <Container>
+        <Image source={logoImg} />
+        <Title>Faça seu logon</Title>
+        <Input name="email" icon="mail" placeholder="Email" />
+        <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button
+        <Button
+          onPress={() => {
+            console.log("OK");
+          }}
+        >
+          Entrar
+        </Button>
+
+        <ForgotPassword
+          onPress={() => {
+            console.log("esqueci a senha");
+          }}
+        >
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+      <CreateAccountButton
         onPress={() => {
-          console.log("OK");
+          console.log("criar conta");
         }}
       >
-        Entrar
-      </Button>
-    </Container>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
