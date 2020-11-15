8140f75dc33a08bd6591289b24426604bdb3f38c
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-11-15 00:47:50 -0300

    using keyboardAvoidingView and ScrollView to keep user eXperience highlander

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Image } from "react-native";
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from "react-native";
 import Icon from "react-native-vector-icons/Feather";
 import logoImg from "../../assets/logo.png";
 import { Input, Button } from "../../components";
@@ -15,28 +21,42 @@ import {
 const SignIn: React.FC = () => {
   return (
     <>
-      <Container>
-        <Image source={logoImg} />
-        <Title>Faça seu logon</Title>
-        <Input name="email" icon="mail" placeholder="Email" />
-        <Input name="password" icon="lock" placeholder="Senha" />
-
-        <Button
-          onPress={() => {
-            console.log("OK");
-          }}
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
         >
-          Entrar
-        </Button>
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="Email" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-        <ForgotPassword
-          onPress={() => {
-            console.log("esqueci a senha");
-          }}
-        >
-          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
-        </ForgotPassword>
-      </Container>
+            <Button
+              onPress={() => {
+                console.log("OK");
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword
+              onPress={() => {
+                console.log("esqueci a senha");
+              }}
+            >
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
       <CreateAccountButton
         onPress={() => {
           console.log("criar conta");
